f0dfebb0fabcd30f4732db6439f54bb92fba5928
hintsete <hintseteabera@gmail.com>
2025-07-30 15:47:52 +0300

    implemented the usecase and entity

---
+++ lib/domain/entities/product.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+
+class Product {
+  int id;
+  String name;
+  String description;
+  String imageURL;
+  Float price;
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.imageURL,
+    required this.price,
+
+  });
+}
